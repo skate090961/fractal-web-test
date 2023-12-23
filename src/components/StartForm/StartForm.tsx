1d3cadcb875dacb28ff30d11992425e613dff263
skate090961 <m4xwork@yandex.ru>
2023-12-23 17:16:35 +0400

    commit

--- src/components/StartForm/StartForm.tsx
+++ src/components/StartForm/StartForm.tsx
@@ -2,13 +2,12 @@ import s from "./StartForm.module.scss"
 import { ProfileCard } from "../ProfileCard/ProfileCard.tsx"
 import { FormField } from "../FormField/FormField.tsx"
 import { SuperButton } from "../UI/SuperButton/SuperButton.tsx"
-import { Link } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { selectMultiForm } from "../../store/multiForm/multi-form-selectors.ts"
 import * as yup from "yup"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
-import { setName, setNickname, setSername, setSex } from "../../store/multiForm/multi-form-reducer.ts"
 import React from "react"
 
 export type FirstStepFormInput = {
@@ -17,12 +16,13 @@ export type FirstStepFormInput = {
 }
 
 export const StartForm = () => {
+  const navigate = useNavigate()
   const multiFormData = useSelector(selectMultiForm)
   const dispatch = useDispatch()
   const schema = yup
     .object({
       phone: yup.string(),
-      email: yup.string(),
+      email: yup.string().email("Введите корректный email").required("Введите email"),
     })
     .required()
 
@@ -42,10 +42,11 @@ export const StartForm = () => {
   const onSubmit: SubmitHandler<FirstStepFormInput> = (data) => {
     const { phone, email } = data
     console.log(data)
+    navigate("/steps")
   }
 
   return (
-    <div className={s.container}>
+    <form className={s.container} onSubmit={handleSubmit(onSubmit)}>
       <div className={s.wrapper}>
         <div className={s.profileCard}>
           <ProfileCard />
@@ -56,17 +57,23 @@ export const StartForm = () => {
           error={errors.phone}
           label="Номер телефона"
           placeholder="+7 (999) 999-99-99"
+          onChange={(event) => {
+            event.target.value = "1123123"
+          }}
         />
-        {/*<FormField label="Номер телефона" placeholder="+7 999 999-99-99" />*/}
-        {/*<FormField label="Email" placeholder="webstudio.fractal@example.com" />*/}
+        <FormField
+          name="email"
+          register={register}
+          error={errors.email}
+          label="Email"
+          placeholder="webstudio.fractal@example.com"
+        />
       </div>
-      <form className={s.buttonWrapper}>
-        <Link to={"/steps"}>
-          <SuperButton variant={"contained"} type={"submit"}>
-            Начать
-          </SuperButton>
-        </Link>
-      </form>
-    </div>
+      <div className={s.buttonWrapper}>
+        <SuperButton variant={"contained"} type={"submit"}>
+          Начать
+        </SuperButton>
+      </div>
+    </form>
   )
 }
