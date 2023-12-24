1a1ba1cf8e99842816dd783133ca3fd7da14fb93
skate090961 <m4xwork@yandex.ru>
2023-12-25 03:44:28 +0400

    commit

--- src/store/multiForm/multi-form-thunk.ts
+++ src/store/multiForm/multi-form-thunk.ts
@@ -1,6 +1,6 @@
 import { Dispatch } from "redux"
 import { formAPI } from "../../api/form-api.ts"
-import { clearForm, FormDataModelType, setRequestStatus } from "./multi-form-reducer.ts"
+import { clearForm, FormDataModelType, setAdvantageFields, setRequestStatus } from "./multi-form-reducer.ts"
 
 export const postForm = (formData: FormDataModelType) => async (dispatch: Dispatch) => {
   dispatch(setRequestStatus("loading"))
@@ -8,6 +8,7 @@ export const postForm = (formData: FormDataModelType) => async (dispatch: Dispat
     await formAPI.postForm(formData)
     dispatch(setRequestStatus("succeeded"))
     dispatch(clearForm())
+    dispatch(setAdvantageFields([]))
   } catch (e) {
     dispatch(setRequestStatus("failed"))
   }
